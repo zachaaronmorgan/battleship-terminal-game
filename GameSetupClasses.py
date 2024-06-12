ccd6e1df4f4ace6945623673e3e1381ab1e58a9f
Zachary Morgan <zachaaronmorgan@gmail.com>
2024-06-12 12:18:06 -0400

    Created create ship helper function to create a new instance of ship based on given type and created player class

--- GameSetupClasses.py
+++ GameSetupClasses.py
@@ -54,38 +54,70 @@ class Battleship_Board():
         self.state = state
     
     def __repr__(self):
-        return "This is a battleship board"
+        return f"This is an {self.state} battleship board"
+    
+class Friendly_Board(Battleship_Board):
+    def __init__(self, state):
+        super().__init__(state)
+
+class Target_Board(Battleship_Board):
+    def __init__(self, state):
+        super().__init__(state)
 
 # Defines the types of ships available in the game
 class Ship():
-    def __init__(self, ship_type, health, location, status):
+    def __init__(self, ship_type, health, status):
         self.ship_type = ship_type
         self.health = health
-        self.location = location
         self.status = status
 
     def __repr__(self):
-        return f"This is a {self.ship_type} located at {self.location} with {self.health} hits remaining. The current status of this ship is {self.status}" 
+        return f"This is a {self.ship_type} with {self.health} hits remaining. The current status of this ship is {self.status}" 
         
 
 class Carrier(Ship):
-    def __init__(self, location, status):
-        super().__init__(ship_type='Carrier', health=5, location=location, status=status)
+    def __init__(self, status):
+        super().__init__(ship_type='Carrier', health=5, status=status)
         
 class Battleship(Ship):
     def __init__(self, location, status):
-        super().__init__(ship_type='Battleship', health=4, location=location, status=status)
+        super().__init__(ship_type='Battleship', health=4, status=status)
 
 class Cruiser(Ship):
     def __init__(self, location, status):
-        super().__init__(ship_type='Cruiser', health=3, location=location, status=status)
+        super().__init__(ship_type='Cruiser', health=3, status=status)
         
 class Submarine(Ship):
     def __init__(self, location, status):
-        super().__init__(ship_type='Submarine', health=3, location=location, status=status)
+        super().__init__(ship_type='Submarine', health=3, status=status)
 
 class Destroyer(Ship):
     def __init__(self, location, status):
-        super().__init__(ship_type='Destroyer', health=2, location=location, status=status)
+        super().__init__(ship_type='Destroyer', health=2, status=status)
 
+def create_ship(ship_type):
+    if ship_type.lower() == 'carrier':
+        return Carrier('Functional')
+    elif ship_type.lower() == 'battleship':
+        return Battleship('Functional')
+    elif ship_type.lower() == 'battleship':
+        return Cruiser('Functional')
+    elif ship_type.lower() == 'battleship':
+        return Submarine('Functional')
+    else:
+        return Destroyer('Functional')
+class Player():
+    def __init__(self, name, num_ships=5):
+        self.name = name 
+        self.num_ships = num_ships
+        self.friendly_board = Friendly_Board('Active')
+        self.target_board = Target_Board('Active')
+    
+    def __repr__(self):
+        return f"This player's name is {self.name}. They have an {self.friendly_board.state} friendly board and an {self.target_board.state} target board. They currently have {self.num_ships} ships remaining"
+    
+#     def ship_to_board(self, ship_type, start_position, orientation):
+        
 
+# player_1 = Player('Zach')
+# print(player_1.ship_to_board('Carrier'))
