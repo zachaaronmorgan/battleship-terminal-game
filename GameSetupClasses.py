ac89f5234274f40d233fc54cb0d4f4661ddd14cd
Zachary Morgan <zachaaronmorgan@gmail.com>
2024-06-12 10:47:45 -0400

    Created function to check the possible orientations the ship can be on the board based on position and size

--- GameSetupClasses.py
+++ GameSetupClasses.py
@@ -12,9 +12,33 @@ def label_to_coord(label_val):
     column = letter_labels_indices.index(label_val[0])+1
     return row, column
 
-def check_orientations(ship_type, start_position):
+def check_orientations(size, start_position):
     possible_orientations = []
-    
+    orientations = ['up', 'down', 'right', 'left']
+    row, column = label_to_coord(start_position)
+    maximum_val, minimum_val = 10, 1
+    for orientation in orientations:
+        if orientation == 'up':
+            if row - size >= minimum_val:
+                possible_orientations.append(orientation)
+            else:
+                continue 
+        elif orientation == 'down':
+            if row - size <= maximum_val:
+                possible_orientations.append(orientation)
+            else:
+                continue
+        elif orientation == 'right':
+            if column + size <= maximum_val:
+                possible_orientations.append(orientation)
+            else:
+                continue
+        elif orientation == 'left':
+            if column - size >= minimum_val:
+                possible_orientations.append(orientation)
+            else:
+                continue
+    return possible_orientations
 
 class Battleship_Board():
 
