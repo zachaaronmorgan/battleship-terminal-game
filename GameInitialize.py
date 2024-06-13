be8b07b0cdf8a266f8a0866a11caaad6537373e9
Zachary Morgan <zachaaronmorgan@gmail.com>
2024-06-13 19:13:04 -0400

    Added strike function and loop for game to run

--- GameInitialize.py
+++ GameInitialize.py
@@ -1,7 +1,17 @@
 import GameSetupClasses as GSP
-player = GSP.Player("John")
-print(GSP.check_orientations(5, 'B10'))
-player.ship_to_board('carrier', 'B9', 'up')
-player.ship_to_board('battleship','C6', 'right')
-player.display_friendly_board()
-print(player)
+# user_name_1 = input("Hello welcome to my Battleship Terminal Game, to start please enter Player 1 name: ")
+# user_name_2 = input("Hello welcome to my Battleship Terminal Game, to start please enter Player 2 name: ")
+
+player_1 = GSP.Player('Zach')
+player_2 = GSP.Player('John')
+
+player_2.ship_to_board('destroyer', 'D5', 'up')
+player_1.strike(player_2, 'D5')
+player_2.display_friendly_board()
+player_1.display_target_board()
+
+# Loop to run once game has started
+# is_running = True
+# while is_running:
+#     print("Life is fun")
+#     is_running = False
--- GameSetupClasses.py
+++ GameSetupClasses.py
@@ -1,51 +1,12 @@
 # Imports 
-import string
+import HelperFunctionsGame as HFG
 import numpy as np
-
-# Helper Variables/Functions
-letter_labels_orig = np.array(list(string.ascii_uppercase[:10])) 
-letter_labels = np.array([' ' + letter + ' ' for letter in letter_labels_orig])
-letter_labels_indices = [n for n in letter_labels_orig]
-number_labels = np.array(range(1, 11))  
-
-def label_to_coord(label_val):
-    row = int(label_val[1:])
-    column = letter_labels_indices.index(label_val[0])+1
-    return row, column
-
-def check_orientations(size, start_position):
-    possible_orientations = []
-    orientations = ['up', 'down', 'right', 'left']
-    row, column = label_to_coord(start_position)
-    maximum_val, minimum_val = 10, 1
-    for orientation in orientations:
-        if orientation == 'up':
-            if row - size >= minimum_val:
-                possible_orientations.append(orientation)
-            else:
-                continue 
-        elif orientation == 'down':
-            if row + size <= maximum_val:
-                possible_orientations.append(orientation)
-            else:
-                continue
-        elif orientation == 'right':
-            if column + size <= maximum_val:
-                possible_orientations.append(orientation)
-            else:
-                continue
-        elif orientation == 'left':
-            if column - size >= minimum_val:
-                possible_orientations.append(orientation)
-            else:
-                continue
-    return possible_orientations
 
 class Battleship_Board():
     def __init__(self, state):
         self.state = state
         self.board = np.full((11, 11), '   ')
-        self.board[0, 1:] = letter_labels
+        self.board[0, 1:] = HFG.letter_labels
         self.board[1:, 0] = np.array([f'{i} ' if i < 10 else f'{i}' for i in range(1, 11)])
         self.board[0, 0] = '  '
     
@@ -65,6 +26,7 @@ class Ship():
         self.size = size
         self.status = status
         self.s = s
+        self.health = size
 
     def __repr__(self):
         return f"This is a {self.ship_type} with a size of {self.size}. The current status of this ship is {self.status}. The symbol for this ship is {self.s}" 
@@ -101,6 +63,7 @@ def create_ship(ship_type):
     if ship_type.lower() in ships:
         return Ship(*ships[ship_type.lower()])
     raise ValueError("Unknown ship type")
+
 class Player():
     def __init__(self, name, num_ships=5):
         self.name = name 
@@ -112,30 +75,38 @@ class Player():
         return f"This player's name is {self.name}. They have an {self.friendly_board.state} friendly board and an {self.target_board.state} target board. They currently have {self.num_ships} ships remaining"
     
     def display_friendly_board(self):
-        print(self.friendly_board)
+        print(self.friendly_board.board)
     
     def display_target_board(self):
-        print(self.target_board)
+        print(self.target_board.board)
     
     def ship_to_board(self, ship_type, start_position, orientation):
         new_ship = create_ship(ship_type)
-        row, column = label_to_coord(start_position)
+        row, column = HFG.label_to_coord(start_position)
         if orientation.lower() == 'up':
-            self.friendly_board.board[row:row, column] = new_ship.s
+            self.friendly_board.board[row-new_ship.size+1:row+1, column] = new_ship.s
         elif orientation.lower() == 'down':
             self.friendly_board.board[row:row+new_ship.size, column] = new_ship.s
         elif orientation.lower() == 'right':
             self.friendly_board.board[row, column:column+new_ship.size] = new_ship.s
         elif orientation.lower() == 'left':
-            self.friendly_board.board[row, column-new_ship.size:column] = new_ship.s
+            self.friendly_board.board[row, column-new_ship.size+1:column+1] = new_ship.s
+    
+    def strike(self, enemy, location):
+        row, column = HFG.label_to_coord(location)
         
+        if enemy.friendly_board.board[row, column] == ' S ':
+            enemy.friendly_board.board[row, column] = ' H '
+            self.target_board.board[row, column] = ' H '
+            
+        elif enemy.friendly_board.board[row, column] == '   ':
+            enemy.friendly_board.board[row,column] = ' M '
+            self.target_board.board[row, column] = ' M '
+            
+        elif enemy.friendly_board.board[row, column] in [' M ', ' H ']:
+            print("You have already struck here. Please select a new location")
+        
+        
+            
         
 
-player = Player("John")
-player.display_friendly_board()
-print(player)
-print(label_to_coord('A10'))
-print(check_orientations(5, 'B10'))
-player.ship_to_board('carrier', 'B10', 'up')
-player.ship_to_board('battleship','C6', 'right')
-player.display_friendly_board()
