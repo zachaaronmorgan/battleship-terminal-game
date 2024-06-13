fdccdf387ad93e07db3db376474084a7ffdb6a2e
Zachary Morgan <zachaaronmorgan@gmail.com>
2024-06-13 16:29:45 -0400

    Finalized symbols for ships and board display

--- GameSetupClasses.py
+++ GameSetupClasses.py
@@ -3,12 +3,13 @@ import string
 import numpy as np
 
 # Helper Variables/Functions
-letter_labels = np.array(list(string.ascii_uppercase[:10])) 
-letter_labels_indices = [n for n in letter_labels]
+letter_labels_orig = np.array(list(string.ascii_uppercase[:10])) 
+letter_labels = np.array([' ' + letter + ' ' for letter in letter_labels_orig])
+letter_labels_indices = [n for n in letter_labels_orig]
 number_labels = np.array(range(1, 11))  
 
 def label_to_coord(label_val):
-    row = int(label_val[1])
+    row = int(label_val[1:])
     column = letter_labels_indices.index(label_val[0])+1
     return row, column
 
@@ -24,7 +25,7 @@ def check_orientations(size, start_position):
             else:
                 continue 
         elif orientation == 'down':
-            if row - size <= maximum_val:
+            if row + size <= maximum_val:
                 possible_orientations.append(orientation)
             else:
                 continue
@@ -41,71 +42,65 @@ def check_orientations(size, start_position):
     return possible_orientations
 
 class Battleship_Board():
-
-    board = np.full((11, 11), ' ')
-
-    board[0, 1:] = letter_labels
-
-    board[1:, 0] = number_labels
-
-    board[0, 0] = ' '
-    
     def __init__(self, state):
         self.state = state
+        self.board = np.full((11, 11), '   ')
+        self.board[0, 1:] = letter_labels
+        self.board[1:, 0] = np.array([f'{i} ' if i < 10 else f'{i}' for i in range(1, 11)])
+        self.board[0, 0] = '  '
     
     def __repr__(self):
-        return f"This is an {self.state} battleship board"
+        return f"\n{np.array2string(self.board, separator=' ')}\nThis is an {self.state} battleship board."
     
 class Friendly_Board(Battleship_Board):
-    def __init__(self, state):
-        super().__init__(state)
+    pass
 
 class Target_Board(Battleship_Board):
-    def __init__(self, state):
-        super().__init__(state)
+    pass
 
 # Defines the types of ships available in the game
 class Ship():
-    def __init__(self, ship_type, health, status):
+    def __init__(self, ship_type, size, status, s='S'):
         self.ship_type = ship_type
-        self.health = health
+        self.size = size
         self.status = status
+        self.s = s
 
     def __repr__(self):
-        return f"This is a {self.ship_type} with {self.health} hits remaining. The current status of this ship is {self.status}" 
+        return f"This is a {self.ship_type} with a size of {self.size}. The current status of this ship is {self.status}. The symbol for this ship is {self.s}" 
         
 
 class Carrier(Ship):
     def __init__(self, status):
-        super().__init__(ship_type='Carrier', health=5, status=status)
+        super().__init__(ship_type='Carrier', size=5, status=status)
         
 class Battleship(Ship):
-    def __init__(self, location, status):
-        super().__init__(ship_type='Battleship', health=4, status=status)
+    def __init__(self, status):
+        super().__init__(ship_type='Battleship', size=4, status=status)
 
 class Cruiser(Ship):
-    def __init__(self, location, status):
-        super().__init__(ship_type='Cruiser', health=3, status=status)
+    def __init__(self, status):
+        super().__init__(ship_type='Cruiser', size=3, status=status)
         
 class Submarine(Ship):
-    def __init__(self, location, status):
-        super().__init__(ship_type='Submarine', health=3, status=status)
+    def __init__(self, status):
+        super().__init__(ship_type='Submarine', size=3, status=status)
 
 class Destroyer(Ship):
-    def __init__(self, location, status):
-        super().__init__(ship_type='Destroyer', health=2, status=status)
+    def __init__(self, status):
+        super().__init__(ship_type='Destroyer', size=2, status=status)
 
 def create_ship(ship_type):
-    if ship_type.lower() == 'carrier':
-        return Carrier('Functional')
-    elif ship_type.lower() == 'battleship':
-        return Battleship('Functional')
-    elif ship_type.lower() == 'battleship':
-        return Cruiser('Functional')
-    elif ship_type.lower() == 'battleship':
-        return Submarine('Functional')
-    else:
-        return Destroyer('Functional')
+    ships = {
+        'carrier': ('Carrier', 5, 'Functional', ' S '),
+        'battleship': ('Battleship', 4, 'Functional', ' S '),
+        'cruiser': ('Cruiser', 3, 'Functional', 'S'),
+        'submarine': ('Submarine', 3, 'Functional', ' S '),
+        'destroyer': ('Destroyer', 2, 'Functional', ' S ')
+    }
+    if ship_type.lower() in ships:
+        return Ship(*ships[ship_type.lower()])
+    raise ValueError("Unknown ship type")
 class Player():
     def __init__(self, name, num_ships=5):
         self.name = name 
@@ -116,8 +111,31 @@ class Player():
     def __repr__(self):
         return f"This player's name is {self.name}. They have an {self.friendly_board.state} friendly board and an {self.target_board.state} target board. They currently have {self.num_ships} ships remaining"
     
-#     def ship_to_board(self, ship_type, start_position, orientation):
+    def display_friendly_board(self):
+        print(self.friendly_board)
+    
+    def display_target_board(self):
+        print(self.target_board)
+    
+    def ship_to_board(self, ship_type, start_position, orientation):
+        new_ship = create_ship(ship_type)
+        row, column = label_to_coord(start_position)
+        if orientation.lower() == 'up':
+            self.friendly_board.board[row:row, column] = new_ship.s
+        elif orientation.lower() == 'down':
+            self.friendly_board.board[row:row+new_ship.size, column] = new_ship.s
+        elif orientation.lower() == 'right':
+            self.friendly_board.board[row, column:column+new_ship.size] = new_ship.s
+        elif orientation.lower() == 'left':
+            self.friendly_board.board[row, column-new_ship.size:column] = new_ship.s
+        
         
 
-# player_1 = Player('Zach')
-# print(player_1.ship_to_board('Carrier'))
+player = Player("John")
+player.display_friendly_board()
+print(player)
+print(label_to_coord('A10'))
+print(check_orientations(5, 'B10'))
+player.ship_to_board('carrier', 'B10', 'up')
+player.ship_to_board('battleship','C6', 'right')
+player.display_friendly_board()
