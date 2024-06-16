fd2c8f25d6c6171d0390d3e4e2b79ea00a30a582
Zachary Morgan <zachaaronmorgan@gmail.com>
2024-06-16 12:28:47 -0400

    Added loop and functions so that players can each have time to look at their ship placement and reset if desired

--- GameInitialize.py
+++ GameInitialize.py
@@ -6,26 +6,45 @@ from HelperFunctionsGame import multiple_choice, check_orientations, clear_scree
 player_1 = GSP.Player('Zach')
 player_2 = GSP.Player('John')
 ships = ['Carrier', 'Battleship', 'Cruiser', 'Submarine', 'Destroyer']
-players = [player_1, player_2]
+players = [player_1]
 for player in players:
-    ships_available = ships.copy()
-    clear_screen()
-    for ship in ships:
-        ship_choice = multiple_choice("Which ship would you like to place: ", ships_available)
-        ships_available.remove(ship_choice)
-        new_ship = GSP.create_ship(ship_choice)
-        start_position = input("Which position on the board would you like to place it: ")
-        orientations = check_orientations(new_ship.size, start_position)
-        chosen_orientation = multiple_choice("In which orientation would you like to place the ship: ", orientations)
-        while player_1.is_occupied(new_ship.size,start_position,chosen_orientation):
-            print("This spot is occupied by another ship. Please choose a different location")
+    while True:
+        ships_available = ships.copy()
+        clear_screen()
+        for ship in ships:
+            ship_choice = multiple_choice("Which ship would you like to place: ", ships_available)
+            ships_available.remove(ship_choice)
+            new_ship = GSP.create_ship(ship_choice)
             start_position = input("Which position on the board would you like to place it: ")
             orientations = check_orientations(new_ship.size, start_position)
             chosen_orientation = multiple_choice("In which orientation would you like to place the ship: ", orientations)
-            
-        player.place_ship(ship_choice, start_position, chosen_orientation)
-        player.display_friendly_board()
+            while player_1.is_occupied(new_ship.size,start_position,chosen_orientation):
+                print("This spot is occupied by another ship. Please choose a different location")
+                start_position = input("Which position on the board would you like to place it: ")
+                orientations = check_orientations(new_ship.size, start_position)
+                chosen_orientation = multiple_choice("In which orientation would you like to place the ship: ", orientations)
+                
+            player.place_ship(ship_choice, start_position, chosen_orientation)
+            player.display_friendly_board()
+        start_over = input("Would you like to restart the ship placing process (y/n): ").strip().lower()
+        if start_over == 'y':
+            player.remove_ships()
+            continue
+        else:
+            break
+        
 
+# player_1.place_ship('carrier', 'E6', 'right')
+# player_1.place_ship('destroyer', 'J3', 'right')
+# player_1.place_ship('submarine', 'A2', 'down')
+# player_1.place_ship('battleship', 'H5', 'right')
+# player_1.place_ship('cruiser', 'A9', 'up')
+# player_1.display_friendly_board()
+# # ship_coord = player_1.get_ship_coordinates()
+# # print(ship_coord)
+# player_1.remove_ships()
+
+# player_1.display_friendly_board()
 # # player_2.ship_to_board('destroyer', 'E6', 'right')
 # # player_2.ship_to_board('carrier', 'B6', 'up')
 # # player_1.strike(player_2, 'D5')
